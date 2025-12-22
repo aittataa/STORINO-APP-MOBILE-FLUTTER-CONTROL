5f4f34955952f6805334e10344bbd9502f8d488e
aittataa <aittata.abderrahim@gmail.com>
2025-12-23 00:12:36 +0100

    refactor(navigation): Remove unused routes and imports
    Removes routes and associated page definitions that are no longer in use, simplifying the application's navigation structure.
    
    This includes:
    - Deleting the `PRODUCTS`, `DETAILS`, and `VALIDATIONS` routes from `app_routes.dart` and `app_pages.dart`.
    - Removing the corresponding `GetPage` definitions and their associated view/binding imports from the `AppPages.routes` list.
    - Cleaning up unused import statements.

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,19 +1,12 @@
 import "package:flutter/material.dart";
-
 import "package:get/get.dart";
 
-import "../modules/details/bindings/details_binding.dart";
-import "../modules/details/views/details_view.dart";
 import "../modules/initial/bindings/initial_binding.dart";
 import "../modules/initial/views/initial_view.dart";
-import "../modules/products/bindings/products_binding.dart";
-import "../modules/products/views/products_view.dart";
 import "../modules/register/bindings/register_binding.dart";
 import "../modules/register/views/register_view.dart";
 import "../modules/splash/bindings/splash_binding.dart";
 import "../modules/splash/views/splash_view.dart";
-import "../modules/validations/bindings/validations_binding.dart";
-import "../modules/validations/views/validations_view.dart";
 
 part "app_routes.dart";
 
@@ -37,25 +30,10 @@ class AppPages {
       binding: RegisterBinding(),
       page: () => const RegisterView(),
     ),
-    GetPage(
+    GetPage<Widget>(
       name: _Paths.INITIAL,
-      page: () => const InitialView(),
       binding: InitialBinding(),
-    ),
-    GetPage(
-      name: _Paths.PRODUCTS,
-      page: () => const ProductsView(),
-      binding: ProductsBinding(),
-    ),
-    GetPage(
-      name: _Paths.DETAILS,
-      page: () => const DetailsView(),
-      binding: DetailsBinding(),
-    ),
-    GetPage(
-      name: _Paths.VALIDATIONS,
-      page: () => const ValidationsView(),
-      binding: ValidationsBinding(),
+      page: () => const InitialView(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -6,9 +6,6 @@ abstract class Routes {
   static const String SPLASH = _Paths.SPLASH;
   static const String REGISTER = _Paths.REGISTER;
   static const String INITIAL = _Paths.INITIAL;
-  static const String PRODUCTS = _Paths.PRODUCTS;
-  static const String DETAILS = _Paths.DETAILS;
-  static const String VALIDATIONS = _Paths.VALIDATIONS;
 }
 
 abstract class _Paths {
@@ -17,7 +14,4 @@ abstract class _Paths {
   static const String SPLASH = "/splash";
   static const String REGISTER = "/register";
   static const String INITIAL = "/initial";
-  static const String PRODUCTS = "/products";
-  static const String DETAILS = "/details";
-  static const String VALIDATIONS = "/validations";
 }
